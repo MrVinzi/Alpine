46e896b1acefc912d01ad252fd0e45e396e60431
MrVinzi <romakelemen@gmail.com>
2021-01-13 19:13:09 +0200

    release_slider

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+var currentPosX = 0;
+
+function clickPrev(e) {
+    var posX = currentPosX + 640;
+    if (posX>(valuePicture()*(-640)) && posX<=0) {
+        currentPosX = posX;
+        setTransition(posX);
+    }
+}
+
+function clickNext(e) {
+    var posX = currentPosX - 640;
+    if (posX>(valuePicture()*(-640)) && posX<=0) {
+    currentPosX = posX;
+    setTransition(posX);
+   }
+    
+}
+
+
+
+function setTransition(p) {
+    if (currentPosX>(valuePicture()*(-640)) && currentPosX<=0) {
+    var elm = document.getElementById('banner_list');
+    elm.style.transform = "translateX(" + p + "px)";
+    console.log("translateX(" + p + ")");
+    } 
+}
+
+function valuePicture() {
+    var q = document.getElementsByClassName('banner-item');
+    l = q.length;
+    return l;
+}
